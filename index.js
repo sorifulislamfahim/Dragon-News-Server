a8a13c13f8e7e9b5ffaba25e067f8577d86c7e75
Soriful Islam Fahim <islamsoriful248@gmail.com>
2022-10-25 10:35:53 +0600

    added news server

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors())
 
-const catagorys = require('./data/catagorys.json')
+const catagorys = require('./data/catagorys.json');
+const news = require('./data/news.json');
 
 app.get('/', (req, res) => {
     res.send('NEWS API Running');
@@ -13,6 +14,27 @@ app.get('/', (req, res) => {
 
 app.get('/news-catagories', (req, res) => {
     res.send(catagorys)
+})
+
+app.get('/catagory/:id', (req, res) => {
+    const id = req.params.id;
+    if(id === '08'){
+        res.send(news)
+    }
+    else{
+        const catagoryId = news.filter(n => n.category_id === id);
+        res.send(catagoryId);
+    }
+})
+
+app.get('/news', (req, res) => {
+    res.send(news)
+})
+
+app.get('/news/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedNews = news.find( n => n._id === id);
+    res.send(selectedNews);
 })
 
 app.listen(port, () => {
